7e3d4d3bf800b14465b81d9b2bf0d9c087f11175
Admir Trakic <abadta@almbrand.dk>
2022-11-18 11:52:14 +0100

    Update

--- main.py
+++ main.py
@@ -14,13 +14,16 @@ def main():
     environment = Environment(loader=FileSystemLoader("templates/"))
     template = environment.get_template("message.txt")
     for player in players:
+        filename = f"dist/{player['name'].lower()}.txt"
         content = template.render(
             player, 
             max_score=100,
             test_name="DevOps challenge",
             author="Admin"
             )
-        print(f"{content}")
+    with open(filename, mode="w", encoding="utf-8") as message:
+        message.write(content)
+        print(f"... wrote {filename}")
 
 if __name__ == "__main__":
     main()
