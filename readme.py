479ee471425849919c90f4986a96f1b5c60280df
Admir Trakic <abadta@almbrand.dk>
2022-11-18 10:51:13 +0100

    Update

---
+++ readme.py
@@ -0,0 +1,27 @@
+from jinja2 import Environment, FileSystemLoader
+
+players = [
+    {"name": "Foo",  "score": 100},
+    {"name": "Bar", "score": 87},
+    {"name": "Baz", "score": 40},
+    {"name": "Unknown", "score": 75},
+]
+
+test_name = "Python Challenge"
+max_score = 100
+environment = Environment(loader=FileSystemLoader("templates/"))
+template = environment.get_template("message.txt")
+
+def main():
+    for player in players:
+        filename = f"message_{player['name'].lower()}.txt"
+        content = template.render(
+            player, 
+            max_score=max_score,
+            test_name=test_name,
+            author="Admin"
+            )
+    print(f"{content}")
+
+if __name__ == "__main__":
+    main()
